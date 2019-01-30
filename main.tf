72abde7e35ce778e85ad914294be4dc2e88ba2f5
Danish Siddiqui <danishsiddiqui09@gmail.com>
2019-01-30 12:37:02 +0000

    did nothing, just tidied up

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ provider "aws" {
   region = "eu-west-1"
 }
 
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  default = 8080
+}
+
 resource "aws_instance" "example" {
   ami = "ami-09f0b8b3e41191524"
   instance_type = "t2.micro"
@@ -10,7 +15,7 @@ resource "aws_instance" "example" {
   user_data = <<-EOF
               #!/bin/bash
               echo "HelloWorld" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p "${var.server_port}" &
               EOF
 
 
@@ -23,10 +28,14 @@ resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port = 8080
-    to_port = 8080
+    from_port = "${var.server_port}"
+    to_port = "${var.server_port}"
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+}
+
+output "public_ip" {
+  value = "${aws_instance.example.public_ip}"
 }
